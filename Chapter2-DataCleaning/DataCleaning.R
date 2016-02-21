166c7b91150e0f92fd75562a3037f9c466f1597c
jgendron <gerald.gendron@gmail.com>
2016-02-21 15:18:32 -0500

    Modified code to Hackley Wickham style

--- Chapter2-DataCleaning/DataCleaning.R
+++ Chapter2-DataCleaning/DataCleaning.R
@@ -14,22 +14,22 @@ str(bike)
 table(is.na(bike))
 
 library(stringr)
-str_detect(bike,"NA")
+str_detect(bike, "NA")
 table(is.na(bike$sources))
 
-bad_data <- str_subset(bike$humidity,"[a-z A-Z]")
-location <- str_detect(bike$humidity,bad_data)
-bike[location,]
+bad_data <- str_subset(bike$humidity, "[a-z A-Z]")
+location <- str_detect(bike$humidity, bad_data)
+bike[location, ]
 
-bike$humidity <- str_replace_all(bike$humidity,bad_data,"61")
-bike[location,]
+bike$humidity <- str_replace_all(bike$humidity, bad_data, "61")
+bike[location, ]
 
 #
 #  Converting inputs to data types suited for analysis
 bike$humidity <- as.numeric(bike$humidity)
 
-categorical <- c("season","holiday","workingday","weather")
-bike[,categorical] <- lapply(bike[,categorical], factor)
+categorical <- c("season", "holiday", "workingday", "weather")
+bike[ ,categorical] <- lapply(bike[ ,categorical], factor)
 
 library(lubridate)
 bike$datetime <- mdy_hm(bike$datetime)
@@ -48,7 +48,7 @@ unique(bike$sources)
 library(DataCombine)
 web_sites <- "(www.[a-z]*.[a-z]*)"
 current <- unique(str_subset(bike$sources, web_sites))
-replace <- rep("web",length(current))
+replace <- rep("web", length(current))
 replacements <- data.frame(from = current, to = replace)
 bike <- FindReplace(data = bike, Var = "sources", replacements,
                     from = "from", to = "to", exact = FALSE)
