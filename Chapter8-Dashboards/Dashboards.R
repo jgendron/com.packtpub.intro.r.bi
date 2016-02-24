dbf34a73a95be3890841cb35f013d1e3f79a613c
Steven Mortimer <reportmort@gmail.com>
2016-02-24 18:29:08 -0500

    Add Outline for Chapter 8
    Add an initial list of topics in an R file that we can expand upon in the book

---
+++ Chapter8-Dashboards/Dashboards.R
@@ -0,0 +1,25 @@
+# Copyright 2016 Packt Publishing
+
+# Introduction to R for Business Intelligence
+# Chapter 8 - Publishing Data Products as Dashboards
+
+# topics:
+
+# skeleton - (including a www folder)
+
+# setting the header - tags$head()
+
+# setting the body - mainPanel?
+
+# using a global.R - load packages
+
+# modularizing the ui - separate .R files for the ui and source them
+
+# variables outside server session - in the top of server.R
+
+# variables inside server session - inside the top of server.R
+
+# shinyjs?
+
+# deploy? put on shinyapps.io
+
