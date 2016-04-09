5891b536ae55228c83e95fcdc1ccdb2aff03c21e
Steven Mortimer <reportmort@gmail.com>
2016-04-09 13:54:33 -0400

    Finish Styling
    Add styles to the table of data

--- Chapter8-Dashboards/campaign-creator-app/server.R
+++ Chapter8-Dashboards/campaign-creator-app/server.R
@@ -4,6 +4,9 @@
 # any variables declared here will be shared 
 # between all Shiny app users
 market <- read.csv("./data/market-data.csv")
+market$`First Name` <- rep('Test', nrow(market))
+market$`Last Name` <- rep('Test', nrow(market))
+market$`Email` <- rep('Test@gmail.com', nrow(market))
 
 shinyServer(function(input, output, session) {
   
@@ -104,32 +107,56 @@ shinyServer(function(input, output, session) {
     print(p)
   })
   
+  table_data <- reactive({
+    table_data <- clustered_dataset() %>% 
+      group_by(cluster_id) %>% 
+      mutate(age_ptile=round(rank(age)/length(age),4), 
+             income_ptile=round(rank(income)/length(income),4), 
+             income=round(income,2)) %>%
+      ungroup() %>%
+      arrange(-income_ptile) %>%
+      select(`First Name`, `Last Name`, 
+             `Email`, `Age`=age, `Income`=income, 
+             `Age %Tile within Cluster`=age_ptile, 
+             `Income %Tile within Cluster`=income_ptile, 
+             `Cluster Id`=cluster_id)
+    return(table_data)
+  })
+  
   # create a table to display potential
   # campaign members sorted by cluster
   output$campaign_table <- DT::renderDataTable({
-
-    d <- DT::datatable(clustered_dataset(), 
+  
+    d <- DT::datatable(table_data(), 
                        options = list(
+                         columnDefs = list(list(width = '150px', targets = 0), 
+                                           list(width = '50px', targets = c(3,7)),
+                                           list(width = '110px', targets = c(4,5,6)),
+                                           list(className = 'dt-center', targets = 3:7)),
                          deferRender = TRUE,
                          autoWidth = FALSE,
                          lengthChange = FALSE,
-                         searching = FALSE,
+                         searching = TRUE,
                          paginate = TRUE,
                          pageLength = 25,
                          info = TRUE,
-                         ordering = TRUE
+                         ordering = TRUE,
+                         order = list(list(7, 'asc'))
                        ),
                        filter = 'top',
                        selection = 'none',
                        class = 'cell-border stripe', 
                        rownames = FALSE,
-                       escape = FALSE)
+                       escape = FALSE
+                       )
     
-    d <- d %>% formatCurrency(c(2), currency="", digits=0)
-
+    d <- d %>% formatCurrency('Income', digits=0) %>%
+      formatPercentage(c('Age %Tile within Cluster', 
+                         'Income %Tile within Cluster'), digits=1)
+    
     return(d)
   })
-  
+
   # provide a server-side method for handling user
   # downloads from the table of data
   output$downloadDataFromTable <- downloadHandler(
@@ -137,7 +164,7 @@ shinyServer(function(input, output, session) {
       paste0('CAMPAIGN_DATA_', format(Sys.Date(), '%Y-%m-%d'), '.csv')
     }
     , content = function(file) {
-      write.csv(iris, file, row.names=FALSE)
+      write.csv(table_data()[input$campaign_table_rows_all,], file, row.names=FALSE)
     }
     , contentType = "text/csv"
   )
--- Chapter8-Dashboards/campaign-creator-app/ui.R
+++ Chapter8-Dashboards/campaign-creator-app/ui.R
@@ -20,45 +20,79 @@ shinyUI(
       # consistent with RStudio projects
       tags$link(rel='stylesheet', 
                 type='text/css', 
-                href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600'),
+                href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600'
+                ),
       
       # load a style sheet specific to this application
       tags$link(rel='stylesheet', 
                 type='text/css', 
-                href='app-styling.css'),
+                href='app-styling.css'
+                ),
       
       # include a browser shortcut icon
       # to make the app more visible in the 
       # browser window and bookmarks folder
       tags$link(rel="shortcut icon", 
                 type="image/x-icon", 
-                href="bike-shortcut-icon.ico")
-    ), 
+                href="bike-shortcut-icon.ico"
+                )
+      ),
     
     # include a progress wheel whenever the app
     # is performing a calculation longer than 1 second
     busyIndicator(text = "Calculation in progress ... ", wait = 0),
     
+    br(),
+    
     fluidRow(
-      h5('Hellow'),
-      sliderInput("cluster_count",
-                  label="How Many Clusters?",
-                  min=2, max=10, 
-                  value=6, step=1)
+      class='vertical-align',
+      column(3, 
+        p(strong('Hello'), ', this application allows you to specify different clusters of customers', 
+          'based on age and income to determine more targeted marketing segments. Simply, ', 
+          'move the slider at right for more or less clusters.'), 
+        p('After choosing a cluster scheme', 
+          'you can filter and download customer data based on those clusters to run a campaign.')
+      ),
+      column(8,
+        sliderInput("cluster_count",
+                   label="How Many Clusters?",
+                   min=2, max=10, 
+                   value=6, step=1)
+      )
     ),
     
     fluidRow(
       column(7, plotOutput("cluster_viz", height = "500px")),
       column(5, 
              h3('Cluster Summary Table'),
-             hr(),
              DT::dataTableOutput("campaign_summary_table", width = "100%"))
+    ),
+    
+    hr(),
+    
+    h3('Potential Campaign Members'),
+    
+    fluidRow(
+      column(4,
+             p('The data in the table below shows all potential customers, their cluster ', 
+                        'membership, and their relative percentile of age and income in that cluster. ', 
+                        'Use the filter boxes at the top of the table to select even more granular ', 
+                        'sets of customers.')
+             ),
+      column(4,
+             p('For example, if you select the 75th percentile and above ', 
+               'for "Income %Tile within Cluster, then you will be selecting only the top 25% ', 
+               'of incomes from each cluster, which might be a nicely stratified sample of customers ', 
+               'with disposable income relative to their peer group.')
+             ),
+      column(4,
+             tags$div(class="itemright",downloadButton('downloadDataFromTable', 'Download Table Data'))
+             )
     ),
     
     fluidRow(
       column(12, DT::dataTableOutput("campaign_table", width = "100%"))
     )
-    
   )
 )
     
